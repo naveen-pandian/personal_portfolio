3d4175d77696102caeb31ec2b4426b9f695bf161
naveen-pandian <naveenpandianp1@gmail.com>
2024-05-03 13:50:13 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,37 @@
 import express from "express";
+import mongoose from "mongoose";
+
 
 const app = express();
+const port = 3000;
 
+mongoose.connect('mongodb+srv://naveenpandianp2:Naveen%4029@cluster0.bul8bxu.mongodb.net/portfolio_data?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=>{
+    console.log('Connected to MongoDb Atlas');
+})
+.catch((error)=>{
+    console.log('Error connection : ',error);
+}); 
+
+const userSchema = new mongoose.Schema(
+{
+    user : {}
+}
+);
+
+const User = mongoose.model('user',userSchema);
+
+
+//Routes
 app.get('/',(req,res)=>{
-    res.send('Express run in vercel ..  hello !! naveen ');
-});
+    res.send('Hello MongoDB Atlas! and vercel!');
+})
+app.get('/api/portfolio_data',async(req,res)=>{
+    const users = await User.find();
+    res.json(users);
+})
 
-app.listen(5000,()=>{
-    console.log("app is running..");
+//Start the server
+app.listen(port,()=>{
+    console.log('Server is running on port ${port}');
 });
