225ea1b157ac97086f5bc9d45aa2ce629e810c71
naveen-pandian <naveenpandianp1@gmail.com>
2024-05-24 21:13:28 +0530

    final coomit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 
 function App() {
     return (
-        <Router basename='/'>
+        <Router basename='/personal_portfolio'>
         <Routes>
             <Route path="/" element={<Home />} />
             {/* <Route path="/:user" element={<Home />} /> */}
